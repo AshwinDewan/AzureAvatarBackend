c3e6d1ab4f4b29725c466397c8b175f3eed22dc7
AshwinDewan <zeakashwin@gmail.com>
2023-11-09 15:30:15 +0530

    added sample endpoint

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var textToSpeech = require('../helpers/tts');
 
 /* GET home page. */
 router.post('/talk', function(req, res, next) {
-
   textToSpeech(req.body.text, req.body.voice)
   .then(result => {
     res.json(result);    
@@ -12,8 +11,10 @@ router.post('/talk', function(req, res, next) {
   .catch(err => {
     res.json({});
   });
-
-
+});
+router.post('/hello', function(req, res, next) {
+  console.log(req.body)
+  res.json({"respTxt": "This end point has been assecced"});
 });
 
 module.exports = router;
